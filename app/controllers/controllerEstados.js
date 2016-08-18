9f8a922bd66b1deb7d98608a0e011b57715fa4ff
Jose Ferreira <ferreirajosejp@gmail.com>
2016-08-18 18:05:56 -0300

    doc duplicate
    documento duplicados verificação

--- app/controllers/controllerEstados.js
+++ app/controllers/controllerEstados.js
@@ -7,39 +7,26 @@ module.exports = function (app) {
 
 
     controllerEstados.listarTodosEstados = function(req, res){
-        console.log(findCollection());
-
         // Estado.find().exec().then(function (estado) {
         //     res.json(estado);
         // }, function (erro) {
         //     console.error(erro);
         //     res.status(500).json(erro);
         // });
+        //console.log("Tetse: "+findCollection() == "Paraíba");
+        console.log(colors.green(findCollection()));
     };
 
     var findCollection = function(){
-        // var promise = Estado.query().exec().then(function (estado) {
-        // var promise = Estado.find().exec(function (err, estado) {
-            
-        // }, function (erro) {
-        //     console.error(erro);
-        // });
-
+   
         var promise = Estado.find().exec();
-
+    
         promise.then(function(user) {
-          return user;
-        })
-        .then(function(user) {
-          //console.log('Estado : ' + user._id);
-           user.forEach(function(jedi){
+             user.forEach(function(jedi){
                 // console.log(jedi.name);
                 console.log(colors.green(jedi.estado_nome));
             });
-          //console.log(colors.green('Estado : ' + user));
-          // do something with updated user
-        })
-        .catch(function(err){
+        }).catch(function(err){
           // just need one of these
           console.log('error:', err);
         });
@@ -53,6 +40,9 @@ module.exports = function (app) {
 
         var patt = /para(i|í)ba/i;
         var r = patt.test(estado);
+
+        
+
          // Cidade.create({cidade_nome: cidade, cidade_reference: [Estado._id]}, function(err, data){
          //            if (err) return res.json(err);
          //            res.status(201).json(estado);
@@ -62,17 +52,17 @@ module.exports = function (app) {
         //     cidade_reference: [Estado._id];
         // });
 
-        Estado.create(estado)
-            .then(function (estado) {
-                Cidade.create({cidade_nome: req.body.cidade_nome, estado_reference: estado._id}, function(err, data){
-                    if (err) return res.json(err);
-                    res.status(201).json(estado);
-               }); 
-                res.status(201).json(estado);
-            }, function (erro) {
-                console.log(erro);
-                res.status.json(erro);
-            });
+        // Estado.create(estado)
+        //     .then(function (estado) {
+        //         Cidade.create({cidade_nome: req.body.cidade_nome, estado_reference: estado._id}, function(err, data){
+        //             if (err) return res.json(err);
+        //             res.status(201).json(estado);
+        //        }); 
+        //         res.status(201).json(estado);
+        //     }, function (erro) {
+        //         console.log(erro);
+        //         res.status.json(erro);
+        //     });
 
             // var estado = req.body.estado_nome;
         // var cidade = req.body.cidade_nome;
